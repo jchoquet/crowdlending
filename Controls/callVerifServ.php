b6637fa1038bedc9284c0dc98d62649c86eafcb6
Mehdi Khadir <Mehdi Khadir>
2016-11-07 00:30:36 +0100

    ajout de la fonctionnalité de recherche de l'id de la commune, avec la vérification des champs rentrés dans le formulaire d'inscription

--- Controls/callVerifServ.php
+++ Controls/callVerifServ.php
@@ -12,21 +12,48 @@ if (verifFullfill() == 0 && verifPassword() && verifEmail() == 0 && verifUsernam
 {
     global $DB, $password, $username, $nom, $prenom, $password2, $phone, $address, $email;
 
-    // A CHANGER AU PLUS VITE
-    $hashpassword = $password;
+    //Hashage du mot de passe
+    $hashpassword = password_hash($password, PASSWORD_DEFAULT);
 
     // Enregistrement du nouvel utilisateur dans la BD
+
+    //Préparation de la requête d'insertion du nouvel utilisateur
     $sql = $DB->prepare("INSERT INTO utilisateur (username, prenom, nom, email, hash_password, id_commune) VALUES (:username, :prenom, :nom, :email, :hashpassword, :id_commune)");
-    $sql->bindParam(':username', $username);
-    $sql->bindParam(':prenom', $prenom);
-    $sql->bindParam(':nom', $nom);
-    $sql->bindParam(':email', $email);
-    $sql->bindParam(':hashpassword', $hashpassword);
-    $id_commune = $DB->query("SELECT id FROM commune WHERE nom=\'" . $address . "\'")->fetch()['id'];
-    $sql->bindParam(':id_commune', $id_commune);
+    $sql->bindValue(':username', $username, PDO::PARAM_STR);
+    $sql->bindValue(':prenom', $prenom, PDO::PARAM_STR);
+    $sql->bindValue(':nom', $nom, PDO::PARAM_STR);
+    $sql->bindValue(':email', $email, PDO::PARAM_STR);
+    $sql->bindValue(':hashpassword', $hashpassword);
+
+    //Préparation de la requête de recherche de l'id de la ville de l'utilisateur
+    $reqId = $DB -> prepare("SELECT id FROM commune WHERE nom = :address LIMIT 1");
+    $reqId->bindValue(':address','Dijon');
+
+    //Execution et récupération de cet id
+    $reqId->execute();
+
+    if ($reqId->rowCount() > 0)
+    {
+        //Cas où la commune a été trouvée, son id est stocké dans $id_commune
+        $check = $reqId->fetch(PDO::FETCH_ASSOC);
+        $id_commune = $check['id'];
+    }
+    else
+    {
+        //Cas où la commune n'a pas été trouvée
+        print("<p> La commune n'a pas été trouvée </p>");
+        print("<a href=\"../index.php\">Accueil</a>");
+    }
+    $sql->bindValue(':id_commune', $id_commune);
+
+    //Execution de la requête d'enregistrement de l'utilisateur
     $sql->execute();
+
+    //Redirection vers la page d'accueil si tout s'est bien passé
     header(__DIR__ . '/../index.php');
 }
+
+//Redirection vers la page d'inscription si les champs ne sont pas valides
 else header(__DIR__ . '/../Views/inscription.php');
 
 ?>
