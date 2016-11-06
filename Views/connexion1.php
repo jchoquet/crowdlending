b395e1d00471945d56165d71c1577e0cc20ccb83
Mohammed MAHROUS <mohammed.mahrous@ensiie.fr>
2016-11-06 22:12:49 +0100

    connexion1.php

---
+++ Views/connexion1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html >
+<html lang="fr">
+  <head>
+    <meta charset="utf-8"/>
+    <link rel="shortcut icon" href="fonts/icone.ico">
+    <title></title>
+    <!-- pour les moteurs de recherche -->
+    <metaname="description" lang="fr" content="" />
+    <metaname="keywords" lang="fr" content="" />
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- jquery -->
+	<script src="js/jquery_library.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="js/bootstrap.js"></script>
+	<!-- fichier css perso -->
+	<link rel="stylesheet" href="css/connexion.css">
+	<!-- fichier JS validation formulaire -->
+	<script src="js/validate_co1.js"></script>
+</head>
+<body>
+<?php include("formulaireConnexion.php"); ?>
+</body>
+</html>
