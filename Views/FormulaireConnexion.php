e9dddfaad3acd488451027e89ebc6762659a7e37
Mohammed MAHROUS <mohammed.mahrous@ensiie.fr>
2016-11-06 22:12:31 +0100

    formulaire d'inscription

---
+++ Views/FormulaireConnexion.php
@@ -0,0 +1,20 @@
+<div class="container-fluid">
+    <!-- formulaire de connexion -->
+			<div id="co" class="col-md-offset-4 col-md-4">
+				
+				<div class="form-group">
+					<label for="identifiantc">Identifiant</label>
+    				<input class="form-control" id="identifiantc" name="identifiantc" type="text" value="" required/> 
+    				<span class="errors" id="idcerror"></span> 
+    			</div>
+    			
+    			<div class="form-group">
+   				 	<label for="mdp">Mot de passe</label>
+    				<input class="form-control" id="mdpc" type="password" name="Mot de passe" value="" required/>
+    				<span class="errors" id="mdperrorc"></span>
+    			</div>
+    		
+    			<button class="btn btn-default btn-block" type="submit" id="connexion" name="connexion">Se connecter</button>
+    			<span class="errors" id="formerror"></span>
+			</div>
+</div>
