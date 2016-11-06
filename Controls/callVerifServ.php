b58d6825147fb796e7faf44a0ed29ba295613761
Julien ROUSSE <julien.rousse@ensiie.fr>
2016-11-06 22:06:20 +0100

    Update callVerifServ.php

--- Controls/callVerifServ.php
+++ Controls/callVerifServ.php
@@ -5,16 +5,17 @@
  * Date: 06/11/2016
  * Time: 19:09
  */
-include( dirname(__FILE__) . '/verificationServeur.php');
+include __DIR__ . '/verificationServeur.php';
 
-//Redirection vers la page d'accueil si tous les tests sont passés, ou vers la page d'inscription sinon
-if (verifFullfill()==0 && verifPassword() && verifEmail()==0 && verifUsername()==0 ){
+// Redirection vers la page d'accueil si tous les tests sont passés, ou vers la page d'inscription sinon
+if (verifFullfill() == 0 && verifPassword() && verifEmail() == 0 && verifUsername() == 0 )
+{
     global $DB, $password, $username, $nom, $prenom, $password2, $phone, $address, $email;
 
-    //A CHANGER AU PLUS VITE
+    // A CHANGER AU PLUS VITE
     $hashpassword = $password;
 
-    //Enregistrement du nouvel utilisateur dans la BD
+    // Enregistrement du nouvel utilisateur dans la BD
     $sql = $DB->prepare("INSERT INTO utilisateur (username, prenom, nom, email, hash_password) VALUES (:username, :prenom, :nom, :email, :hashpassword)");
     $sql->bindParam(':username', $username);
     $sql->bindParam(':prenom', $prenom);
@@ -22,8 +23,8 @@ if (verifFullfill()==0 && verifPassword() && verifEmail()==0 && verifUsername()=
     $sql->bindParam(':email', $email);
     $sql->bindParam(':hashpassword', $hashpassword);
     $sql->execute();
-    header( dirname(__FILE__) . '/../index.php');
+    header(__DIR__ . '/../index.php');
 }
-else header(dirname(__FILE__) . '/../Views/inscription.php');
+else header(__DIR__ . '/../Views/inscription.php');
 
 ?>
