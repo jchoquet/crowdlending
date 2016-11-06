eac82f97714bc8cf1b1cfaa310e8944dc86f76ab
Mohammed MAHROUS <mohammed.mahrous@ensiie.fr>
2016-11-06 22:01:50 +0100

    Update scriptc1.php

--- Views/scriptc1.php
+++ Views/scriptc1.php
@@ -14,47 +14,5 @@ function connexionUser($db,$id)
     return $stmt->hash_password;
   }
 
-try
-  {
-  
-	/*  $DB = new PDO("pgsql:host=localhost;dbname=pima-proj", "postgres", "root");*/
-	$DB = new PDO($DB_TYPE . ":host=" . $DB_HOST . ";dbname=" . $DB_NAME . ";charset=" . $DB_CHARSET, $DB_USER, $DB_PASSWORD);
-
-	/* Si tous les champs sont renseignés */
-	
-	if(isset($_POST['id']) && isset($_POST['mdp']))
-	{
-
-		$id=$_POST['id'];
-		$mdp=$_POST['mdp'];
-
-		$hash=connexionUser($DB,$id);
-
-		if (password_verify($mdp, $hash)) 
-		{
-			/* L'utilisateur existe */
-
-			session_start();
-			$_SESSION['login']=$id;
-			$_SESSION['pwd']=$hash;
-
-		   	echo "OK";
-		}
-        else 
-        {
-            echo "Erreur identifiant ou mot de passe";
-        } 
-	}
-		
-	/* On ferme la connexion */
-	
-	$DB = null;
-}
-
-catch(PDOException $e)
-{
-	echo "Database Error";
-}
-
 
 ?>
