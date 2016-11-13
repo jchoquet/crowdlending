cc4ec630229668c49f81c417fd14b461edb2846c
Qiuhao <qiuhaoqian@gmail.com>
2016-11-13 01:22:07 +0100

    Add three files:popup_test.html popup.php popup.js

---
+++ Scripts/popup.js
@@ -0,0 +1,8 @@
+/**
+ * Created by qianqiuhao on 16/11/12.
+ */
+jQuery(document).ready(function () {
+    $("#nomObjet,#photoObjet").click(function () {
+        $("#myObject").modal();
+    });
+});
