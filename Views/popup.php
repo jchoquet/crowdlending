23f423dfe6e23da5b3583e0afa792da7a7b5de4d
Mehdi Khadir <Mehdi Khadir>
2016-11-13 23:22:05 +0100

    Fichier de test à enlever, car fichier d'anticipation de la future fonction de Julien. Pour le moment la fonctionnalité permet d'afficher les informations d'un objet donné avec jQuery. Pas encore de slides ou de modification des informations

--- Views/popup.php
+++ Views/popup.php
@@ -15,6 +15,7 @@
     <title>pop up</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="../Styles/bootstrap.css"/>
+    <link rel="stylesheet" href="../Styles/popupObjets.css"/>
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -25,6 +26,7 @@
 
     <script src="../Scripts/bootstrap.js"></script>
 
+    <script src="../Scripts/config.js"></script>
     <script src="../Scripts/popup.js"></script>
 </head>
 
@@ -32,19 +34,26 @@
 <?php include ("popup_test.html"); ?>
 
 <!-- Modal -->
-<div id="myObject" class="modal fade" role="dialog" aria-hidden="true">
+
+<div id="myObject" class="modal fade" role="dialog">
     <div class="modal-dialog">
+
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" id="closePbInscription">&times;</button>
+                <button type="button" class="close" data-dismiss="modal" id="closePopup">&times;</button>
+                <h4 class="modal-title" id="nomObjetPopup"></h4>
             </div>
-            <div class="modal-content">
+            <div class="modal-body">
+                <img id="photoObjetBigger" src="" class="picturePopup"/>
+                <h3>Description :</h3>
+                <p id="descriptionObjet"></p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
         </div>
+
     </div>
 </div>
 
