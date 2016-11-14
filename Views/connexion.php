f5a796bc2771795494bbd8fea1ecd2dbd02342dc
Mohammed MAHROUS <mohammed.mahrous@ensiie.fr>
2016-11-14 16:37:23 +0100

    connexion.php

---
+++ Views/connexion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html >
+<html lang="fr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Connexion</title>
+
+    <!-- pour les moteurs de recherche -->
+    <metaname="description" lang="fr" content="Page de connexion sur le site" />
+    <metaname="keywords" lang="fr" content="cnnexion , pima " />
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="../Styles/bootstrap.css">
+
+	<!-- jquery -->
+	<script src="../Scripts/jquery_library.js"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="../Scripts/bootstrap.js"></script>
+
+	<!-- fichier css perso -->
+	<link rel="stylesheet" href="../Styles/connexion.css">
+
+	<!-- fichier JS validation formulaire -->
+	<script src="../Scripts/validate_connexion.js"></script>
+
+</head>
+<body>
+    
+<?php include("formulaireConnexion.php"); ?>
+</body>
+</html>
