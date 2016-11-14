0e44fdd90d610c1c7ab650ed7c4a1e77b45803ef
Mohammed MAHROUS <mohammed.mahrous@ensiie.fr>
2016-11-14 16:30:19 +0100

    ConnexionUser.php

--- Controls/ConnexionUser.php
+++ Controls/ConnexionUser.php
@@ -1,46 +1,35 @@
 <?php
-function connexionUser($id)
+
+//Fonction retournant le mot de passe d'un utilisateur avec un id donné
+function GetPswUser($db,$id)
   {
-      global $DB;
-    $stmt = $DB->prepare("SELECT hash_password FROM utilisateur WHERE id = :id LIMIT 1");
-    $stmt->bindValue(':id', $id);
-    $stmt->execute();
-
-      if ($stmt->rowCount() > 0)
-      {
-          //Cas où l'utilisateur a été trouvé, on stocke son mot de passe hashé
-          $check = $stmt->fetch(PDO::FETCH_ASSOC);
-          $hashedPw = $check['hash_password'];
-      }
-
-      else
-          $hashedPw = "erreur";
-
-    return $hashedPw;
+  global $DB;
+	$stmt = $db->prepare("SELECT hash_password FROM utilisateur WHERE id=:id" );
+	$stmt->bindValue(':id', $id);
+	$stmt->execute();
+	$stmt->setFetchMode(PDO::FETCH_OBJ);
+  $stmt = $stmt->fetch();
+  return $stmt->hash_password;
   }
-
-//Fonction retournant l'id d'un utilisateur donné
-
-function getIdUser($userName)
+  
+//Fonction retournant l'ID d'un utilisateur avec un username donné  
+function GetIdUser($userName)
 {
-    global $DB;
-    $stmt = $DB->prepare("SELECT id FROM utilisateur WHERE username = :username LIMIT 1");
-    $stmt->bindValue(':username', $userName);
-    $stmt->execute();
-
-    if($stmt->rowCount() > 0)
-    {
-        //Cas où l'utilisateur a été trouvé
-        $check = $stmt->fetch(PDO::FETCH_ASSOC);
-        $id = $check['id'];
-    }
-
-    else
-        {
-        echo "Id non trouvé\n";
-        $id = 0;
-    }
-
-    return $id;
+  global $DB;
+  $stmt = $DB->prepare("SELECT id FROM utilisateur WHERE username = :username LIMIT 1");
+  $stmt->bindValue(':username', $userName);
+  $stmt->execute();
+  if($stmt->rowCount() > 0)
+  {
+      $check = $stmt->fetch(PDO::FETCH_ASSOC);
+      $id = $check['id'];
+  }
+  else
+      {
+      echo "Id non trouvé\n";
+      $id = 0;
+  }
+  return $id;
 }
+
 ?>
