9e67a7caec4842519efb1de0bcd941fa7b31ed13
Julien ROUSSE <julien.rousse@ensiie.fr>
2016-11-14 22:46:55 +0100

    Prise en compte du cas où il n'y a pas d'image pour l'objet (image par défaut)

--- Models/objet.sql
+++ Models/objet.sql
@@ -2,7 +2,7 @@ CREATE TABLE objet(
 id INTEGER PRIMARY KEY AUTO_INCREMENT,
 nom VARCHAR(255) NOT NULL,
 prix FLOAT DEFAULT NULL,
-path_photo VARCHAR(255),
+path_photo VARCHAR(255) DEFAULT "no_image.png",
 id_owner INTEGER,
 isAvailable TINYINT DEFAULT 1,
 description TEXT,
