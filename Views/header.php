a25c47e45046dcd94516a8b84edbc311d4e3bd32
Julie Choquet <julie.choquet@ensiie.fr>
2016-11-15 16:41:04 +0100

    Premiere version du header

---
+++ Views/header.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container-fluid">
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <span class="navbar-brand">Lend it </span>
+    </div>
+
+    <div class="collapse navbar-collapse" id="menu-navbar-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="index.php">Accueil</a></li>
+        <li><a href="#">Rechercher un objet</a></li>
+        <li><a href="#">Faire une demande</a></li>
+        <li><a href="#">Ajouter un objet</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Bidule
+            <span class="caret"></span>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Mon compte</a></li>
+            <li><a href="#">Mes objets</a></li>
+            <li><a href="#">Mon historique</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#"><span class="glyphicon glyphicon-log-in"></span> Déconnexion</a>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
---
+++ Views/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html >
+<html lang="fr">
+  <head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Pour ajouter une icone dans le titre de la page -->
+    <!-- link rel="shortcut icon" href="fonts/icone.ico" -->
+    <title>Lend it</title>
+
+    <!-- Source CSS Bootstrap -->
+    <link rel="stylesheet" href="../Styles/bootstrap.css">
+
+    <!-- Source css pour le design du site -->
+    <link rel="stylesheet" href="../Styles/base.css">
+  </head>
+
+  <body>
+
+    <?php include("header.php"); ?>
+
+    <!-- Source jquery -->
+    <script src="../Scripts/jquery_library.js"></script>
+    <!-- Source JavaScript Bootstrap -->
+    <script src="../Scripts/bootstrap.js"></script>
+
+  </body>
+</html>
