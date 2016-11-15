570944405f84c342ad9ea7e8e85286afe96084d3
Mehdi Khadir <Mehdi Khadir>
2016-11-15 18:24:29 +0100

    css work in progress

--- Views/formulaireConnexion.php
+++ Views/formulaireConnexion.php
@@ -1,22 +1,43 @@
-<div class="container-fluid">
-    <!-- formulaire de connexion -->
-    <div id="co" class="col-md-offset-1 col-md-10">
-        <h1 id="connexionTitre">Connexion</h1>
-        <form role="form" method="post" action="../Controls/ConnexionVerif.php">
-            <div class="form-group">
-                <label for="identifiant">Identifiant</label>
-                <input class="form-control" id="identifiantc" name="username" type="text" value="" required/>
-                <span class="errors" id="idcerror"></span>
-            </div>
+<div class="site-wrapper">
+    <div class="site-wrapper-inner">
 
-            <div class="form-group">
-                <label for="mdp">Mot de passe</label>
-                <input class="form-control" id="mdpc" type="password" name="mdp" value="" required/>
-                <span class="errors" id="mdperrorc"></span>
-            </div>
+        <!-- Flèche retour landing page -->
+        <div id="arrow-container">
+            <a href="../index.php"><i class="material-icons md-36 md-light">arrow_back</i></a>
+        </div>
 
-            <button class="btn btn-default btn-block" type="submit" id="connexion" name="connexion">Se connecter</button>
-            <span class="errors" id="formerror"></span>
+
+        <div class="container">
+            <!-- formulaire de connexion -->
+            <form role="form" method="post" action="../Controls/ConnexionVerif.php">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 id="connexionTitre">Connexion</h1>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="form-group col-md-12">
+                        <label for="identifiant">Identifiant</label>
+                        <input class="form-control" id="identifiantc" name="username" type="text" value="" required/>
+                        <span class="errors" id="idcerror"></span>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="form-group col-md-12">
+                        <label for="mdp">Mot de passe</label>
+                        <input class="form-control" id="mdpc" type="password" name="mdp" value="" required/>
+                        <span class="errors" id="mdperrorc"></span>
+                    </div>
+                </div>
+                <p class="lead">
+                    <button class="btn btn-lg btn-info" type="submit" id="connexion" name="connexion">Se connecter
+                    </button>
+                </p>
+                <span class="errors" id="formerror"></span>
+            </form>
+        </div>
     </div>
-    </form>
 </div>
