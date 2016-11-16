1a1fe20ab2ffd0e4cc13f2d68383368c8cec9912
Julien ROUSSE <julien.rousse@ensiie.fr>
2016-11-16 19:47:07 +0100

    Ajout d'un message "Vous n'avez aucun objet disponible.", ajout d'une vérification pour savoir si la page a été rechargée (dans ce cas, on n'affiche pas le message "L'objet "" n'a pas été supprimé)

--- Views/mesObjets.php
+++ Views/mesObjets.php
@@ -31,6 +31,7 @@ include __DIR__ . "/../Models/mesobjetsM.php";
     <h2>Mes objets</h2>
 
     <?php
+    $pageWasRefreshed = isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL'];
     if (isset($_REQUEST['delete']))
     {
         $id_to_delete = $_REQUEST['delete'];
@@ -39,7 +40,9 @@ include __DIR__ . "/../Models/mesobjetsM.php";
         if (delete_objet($id_to_delete))
             print "L'objet \"$nom\" a bien été supprimé." . "<br>";
         else
-            print "Erreur : l'objet \"$nom\" n'a pas été supprimé.";
+            if (!$pageWasRefreshed)
+                print "Erreur : l'objet \"$nom\" n'a pas été supprimé.";
+        $_REQUEST['delete'] = NULL;
     }
     ?>
 
@@ -60,73 +63,79 @@ include __DIR__ . "/../Models/mesobjetsM.php";
     $num = 0; // "num" sert à créer les numéros des lignes du tableau
     $informations_objets = get_available_objets(); // on récupère les informations des objets disponibles de l'utilisateur
 
-    foreach  ($informations_objets as $infor)
-    {
-        $num += 1;
+    if (sizeof($informations_objets) != 0) {
+        foreach ($informations_objets as $infor) {
+            $num += 1;
 
-        if ($num > $page * $DIV) // si on a atteint les objets de la page suivante, on s'arrête
-            break;
+            if ($num > $page * $DIV) // si on a atteint les objets de la page suivante, on s'arrête
+                break;
 
-        if ($num <= ($page - 1) * $DIV) // si on parcourt les objets des pages précédentes, on continue
-            continue;
+            if ($num <= ($page - 1) * $DIV) // si on parcourt les objets des pages précédentes, on continue
+                continue;
 
-        print "<tr>";
-        print "<td>" . $num . "</td>";
+            print "<tr>";
+            print "<td>" . $num . "</td>";
 
-        $path_photo = "../Images/Objets/" . $infor[0];
-        $nom = $infor[1];
+            $path_photo = "../Images/Objets/" . $infor[0];
+            $nom = $infor[1];
 
-        print "<td>";
-        print "<img src=\"$path_photo\" class=\"img-thumbnail\" alt=\"$nom\" width=\"76\" height=\"59\">"; // on réduit la taille des images
-        print "</td>";
+            print "<td>";
+            print "<img src=\"$path_photo\" class=\"img-thumbnail\" alt=\"$nom\" width=\"76\" height=\"59\">"; // on réduit la taille des images
+            print "</td>";
 
-        print "<td>" . $nom . "</td>";
+            print "<td>" . $nom . "</td>";
 
-        print "<td>";
+            print "<td>";
 
-        if ($num == ($page - 1) * $DIV + 1 and $num == sizeof($informations_objets) and $page != 1)
-        {
-            $newpage = $page - 1;
-            $page_after_delete = "mesObjets.php?page=" . $newpage . "&delete=" . $infor[2];
-        }
-        else
-            $page_after_delete = "mesObjets.php?page=" . $page . "&delete=" . $infor[2];
+            // Si l'utilisateur supprime le dernier objet de la dernière page et qu'il ne restait que cet objet dans cette page,
+            // on revient à la page précédente (sauf si la page actuelle est la page 1)
+            if ($num == ($page - 1) * $DIV + 1 and $num == sizeof($informations_objets) and $page != 1) {
+                $newpage = $page - 1;
+                $page_after_delete = "mesObjets.php?page=" . $newpage . "&delete=" . $infor[2];
+            } else
+                $page_after_delete = "mesObjets.php?page=" . $page . "&delete=" . $infor[2];
 
-        print "<div class=\"btn btn-danger btn-lg\" data-toggle=\"modal\" data-target=\"#myModal\" data-nom=\"$nom\" data-link=\"$page_after_delete\">
-          <span class=\"glyphicon glyphicon-trash\"></span>
-        </div>";
+            print "<div class=\"btn btn-danger btn-lg\" data-toggle=\"modal\" data-target=\"#myModal\" data-nom=\"$nom\" data-link=\"$page_after_delete\">
+              <span class=\"glyphicon glyphicon-trash\"></span>
+            </div>";
 
-        ?>
+            ?>
 
-    <!-- http://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_modal_sm&stacked=h -->
-    <!-- Modal -->
-    <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog">
+            <!-- http://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_modal_sm&stacked=h -->
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog">
 
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Message d'avertissement</h4>
-                </div>
-                <div class="modal-body">
-                    <p>Vous allez supprimer l'objet <?php echo $nom; ?>.</p>
-                    <p>Êtes-vous sûr de vouloir continuer ?</p>
-                </div>
-                <div class="modal-footer">
-                    <a id="link-button" role="button" class="btn btn-default">Supprimer l'objet</a>
-                    <button type="button" class="btn btn-default" data-dismiss="modal" id="dismiss-button">Annuler</button>
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Message d'avertissement</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>Vous allez supprimer l'objet <?php echo $nom; ?>.</p>
+                            <p>Êtes-vous sûr de vouloir continuer ?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <a id="link-button" role="button" class="btn btn-default">Supprimer l'objet</a>
+                            <button type="button" class="btn btn-default" data-dismiss="modal" id="dismiss-button">
+                                Annuler
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-        <script type="text/javascript" src="../Scripts/mesObjetsS.js"></script>
+            <script type="text/javascript" src="../Scripts/mesObjetsS.js"></script>
 
-        <?php
+            <?php
 
-        print "</tr>";
+            print "</tr>";
+        }
     }
+
+    else
+        print "Vous n'avez aucun objet disponible.";
     ?>
 
     </tbody>
