e8d252aa401eb0047a3d49b42d0ffaa0d9bca80c
Julie Choquet <julie.choquet@ensiie.fr>
2016-11-20 21:46:41 +0100

    stylesheet

--- Views/mesObjets.php
+++ Views/mesObjets.php
@@ -17,11 +17,19 @@ include "Models/mesobjetsM.php";
     <title>Lend it - Mes objets</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- Source jquery -->
+    <script src="Scripts/jquery_library.js"></script>
+    <!-- Source JavaScript Bootstrap -->
+    <script src="Scripts/bootstrap.js"></script>
+
+    <!-- Source CSS Bootstrap -->
+    <link rel="stylesheet" href="Styles/bootstrap.css">
+
     <!-- Source css pour le design du site -->
     <link rel="stylesheet" href="Styles/base.css">
+    <!-- Source css pour cette page -->
+    <link rel="stylesheet" href="Styles/mesObjets.css">
 </head>
 <body>
 
