62178b9fee1d320f596375d5b38973c87e31847e
Mohammed MAHROUS <mohammed.mahrous@ensiie.fr>
2016-11-26 21:08:30 +0000

    Modification d'objet

--- Scripts/mesObjetsS.js
+++ Scripts/mesObjetsS.js
@@ -30,31 +30,6 @@ $(document).ready(function ()
 });
 
 
-$(document).ready(function ()
-{
-    $('#modifObj').on('show.bs.modal', function (event)
-    { // id of the modal with event
-        var button = $(event.relatedTarget); // Button that triggered the modal
-
-        var link = button.data('link'); // Extract info from data-* attributes
-        var nom = button.data('nom');
-
-        var content = 'Vous allez modifier l\'objet "' + nom + '". Voulez-vous continuer ?';
-
-        // Update the modal's content.
-        var modal = $(this);
-        modal.find('.modal-body').text(content);
-
-
-        // on modifie la page lorsque l'utilisateur clique sur le boutton "Supprimer l'objet"
-        $('#link-button').click(function()
-        {
-            var button = $(this);
-            window.location = link;
-        });
-    });
-});
-
 // Fermeture du popup si l'utilisateur appuie sur la touche "echap"
 $(document).keyup(function(e)
 {
