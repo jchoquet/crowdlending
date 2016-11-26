5a6f7cc4f3c23d2df7a5a1873e217221fb95f2ae
Mohammed MAHROUS <mohammed.mahrous@ensiie.fr>
2016-11-26 22:45:51 +0000

    modification d'objet

--- Models/mesobjetsM.php
+++ Models/mesobjetsM.php
@@ -8,10 +8,10 @@
 
 
 include __DIR__."/connexion.php";
-// L'id de l'utilisateur connectÃ© est connu grÃ¢ce Ã  session_start() lancÃ© au moment de la connexion
+// L'id de l'utilisateur connecté est connu grà¢ce à  session_start() lancé au moment de la connexion
 
 
-// Renvoie le nom de l'objet qui va Ãªtre modifiÃ©, d'id "id_to_modifier"
+// Renvoie le nom de l'objet qui va être modifié, d'id "id_to_modifier"
 function objet_to_modifier($id_to_modifier)
 {
     global $DB;
@@ -20,11 +20,11 @@ function objet_to_modifier($id_to_modifier)
 
 //modifier l'objet d'id "id_to_delete"
 
-function modifier_objet($id_to_modifier)
+function modifier_objet($id_to_modifier , $titre , $prix , $isAvailable , $path_photo , $description )
 {
     global $DB;
     $id = $_SESSION['login'];
-    $qr = $DB->prepare("UPDATE objet SET id=\"$id_to_modifier\" , nom =':titre', prix ='0', path_photo = ':path_photo' ,  id_owner=\"$id\" , isAvailable ='1' ,description = ':description' WHERE id=\"$id_to_modifier\"");
+    $qr = $DB->prepare("UPDATE objet SET id=\"$id_to_modifier\" , nom =\"$titre\", prix =\"$prix\", path_photo = \"$path_photo\" ,  id_owner=\"$id\" , isAvailable =\"$isAvailable\" ,description = \"$description\" WHERE id=\"$id_to_modifier\"");
     $result = $qr->execute();
     return $result;
 }
