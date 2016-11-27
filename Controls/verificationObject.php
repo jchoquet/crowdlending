0a28a3cd8f00a63550b54d17e0cdaafdba65dc4a
Thibault Louet <thibault.louet22@gmail.com>
2016-11-27 01:01:52 +0100

    Changement de la verification du formulaire d'ajou
    Ajoute verifPhoto et le remplissage de categorisation. categorisation a
    testé et photo a réglé (la variable $_FILES ne se remplie pas.

--- Controls/verificationObject.php
+++ Controls/verificationObject.php
@@ -5,22 +5,23 @@ session_start();
 
 $titre = $_POST['titre'];
 $description = $_POST['description'];
-
 $id = $_SESSION['login'];
 
-if (verifFullfill()== 0 && verifTitre()== 0 && verifDescription()== 0)
+if (verifFullfill()== 0 && verifTitre()== 0 && verifDescription()== 0 && verifPhoto()==0)
 {
     global $DB;
     $titre = $DB->quote(htmlspecialchars($_POST['titre']));
     $description = $DB->quote(htmlspecialchars($_POST['description']));
     $owned = $_SESSION['login'];
-    //$id = max_id() + 1 ;
-    $id=102;
-    $path_photo = 'photo';
+	if(isset($_FILES['photo']['tmp_name'])){
+		$nom = md5(uniqid(rand(), true));
+		$extension_upload = strtolower(  substr(  strrchr($_FILES['photo']['name'], '.')  ,1)  );
+		$path_photo = "Images/Objets/".$nom.$extension_upload;
+		$moving = move_uploaded_file($_FILES['photo']['tmp_name'],"../".$nom);
+	}
     $isAvailable = 1;
     $prix = 0;
-    $sql = $DB->prepare("INSERT INTO objet (id ,nom,prix, path_photo, id_owner, isAvailable, description) VALUES (:id , :titre, :prix , :path_photo , :owned, :isAvailable, :description) ON DUPLICATE KEY UPDATE id = :id");
-    $sql->bindValue(':id', $id , PDO::PARAM_INT);
+    $sql = $DB->prepare("INSERT INTO objet (nom,prix, path_photo, id_owner, isAvailable, description) VALUES (:titre, :prix , :path_photo , :owned, :isAvailable, :description);");
     $sql->bindValue(':titre',$titre, PDO::PARAM_STR);
     $sql->bindValue(':prix', $prix , PDO::PARAM_INT);
     $sql->bindValue(':path_photo', $path_photo, PDO::PARAM_STR);
@@ -33,13 +34,34 @@ if (verifFullfill()== 0 && verifTitre()== 0 && verifDescription()== 0)
     //Redirection vers la page d'accueil si tout s'est bien passÃ©
     if($result)
     {
+		if(isset($_POST['categorie'])){
+			$queryObjet = $DB->prepare("SELECT id FROM objet WHERE id_owner=:owned AND nom=:titre;")
+			$queryObjet->bindValue(':owned',$owned, PDO::PARAM_INT);
+			$queryObjet->bindValue(':titre',$titre, PDO::PARAM_STR);
+			$queryObjet->execute();
+			$result = $queryObjet->fetch();
+			$idobjet = $result['id'];
+			
+			
+			$querycate = $DB->prepare("SELECT id FROM categorie WHERE nom=:titre;")
+			$querycate->bindValue(':titre',$_POST['categorie'], PDO::PARAM_STR);
+			$querycate->execute();
+			$result = $querycate->fetch();
+			$idcategorie = $result['id'];
+			
+			
+			$query = $DB->prepare("INSERT INTO categorisation (id_categorie,id_objet) VALUES (:id_categorie, :id_objet);");
+			$query->bindValue(':id_categorie',$idcategorie, PDO::PARAM_STR);
+			$query->bindValue(':id_objet', $idobjet, PDO::PARAM_INT);
+			$query->execute();
+		}
         // DÃ©but de la session
         session_start ();
 
         // Redirection vers la page mesObjets si tout s'est bien passé
         echo "L'ajout s'est bien déroulé.\n";
-        echo "<a href=\"../Views/mesObjets.php\">Ma liste d'objets</a>";
-        header('location: ../Views/mesObjets.php');
+        echo "<a href=\"../mesObjets.php\">Ma liste d'objets</a>";
+        header('location: ../mesObjets.php');
     }
     else
     {
@@ -95,6 +117,42 @@ function max_id()
     $req = $DB->prepare("SELECT MAX(id) from objet");
     $result = $req->execute();
     return $result;
+}
+
+function verifPhoto()
+{
+	if(isset($_FILES['photo']))
+	{
+		if ($_FILES['photo']['error'] > 0)
+		{
+			return 1;
+		}
+		
+		$maxsize=100000;
+		if ($_FILES['photo']['size'] > $maxsize)
+		{
+			return 1;
+		}
+		
+		$extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+		$extension_upload = strtolower(  substr(  strrchr($_FILES['photo']['name'], '.')  ,1)  );
+		if (!in_array($extension_upload,$extensions_valides))
+		{ 
+			return 1;
+		}
+		
+		$image_dim = getimagesize($_FILES['photo']['tmp_name']);
+		$maxwidth=64;
+		$maxheight=64;
+		if ($image_sizes[0] > $maxwidth OR $image_sizes[1] > $maxheight)
+		{
+			return 1;
+		}
+		
+		return 0;
+		
+	}
+	else return 0;
 }
 
 ?>
