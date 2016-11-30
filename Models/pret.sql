e2138a69f0f0f4472807cb54292a542aa5038524
Julien ROUSSE <julien.rousse@ensiie.fr>
2016-11-30 17:10:50 +0100

    Modification de la base de données pour connaître la date de refus de prêt d'un objet

--- Models/pret.sql
+++ Models/pret.sql
@@ -2,8 +2,11 @@ CREATE TABLE pret(
 id INTEGER PRIMARY KEY AUTO_INCREMENT,
 id_borrower INTEGER,
 id_objet INTEGER,
-accepted DATE DEFAULT NULL,
-returned DATE DEFAULT NULL,
+isAccepted TINYINT DEFAULT 0,
+date_accepted DATE DEFAULT NULL,
+date_refused DATE DEFAULT NULL,
+date_returned DATE DEFAULT NULL,
+CONSTRAINT check_pret_isAccepted CHECK (isAccepted=-1 OR isAccepted=0 OR isAccepted=1),
 CONSTRAINT fk_pret_borrower FOREIGN KEY (id_borrower) REFERENCES utilisateur(id) ON DELETE CASCADE,
 CONSTRAINT fk_pret_objet FOREIGN KEY (id_objet) REFERENCES objet(id) ON DELETE CASCADE
 );
