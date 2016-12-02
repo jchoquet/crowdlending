0c34086ab6ccbbaf31772ab250011bcc5f880a56
Julien ROUSSE <julien.rousse@ensiie.fr>
2016-12-02 16:48:58 +0100

    Correction du bug de la page d'inscription

--- Controls/inscription.php
+++ Controls/inscription.php
@@ -13,15 +13,15 @@ include __DIR__ . '/../Views/inscriptionView.php';
 
 //Récupération du type d'erreur s'il y en a une
 
-$erreur = $_GET['err'];
+$erreur = isset($_GET['err']) ? $_GET['err'] : null;
 
 //Si la commune n'a pas été retrouvée dans la BD
-if (isset($erreur) && $erreur == "commune") {
+if ($erreur && $erreur == "commune") {
     echo "<script> communeNotFound(); </script>";
 }
 
 //Autre erreur inconnue
-if (isset($erreur) && erreur == "inconnue") {
+if ($erreur && $erreur == "inconnue") {
     echo "<script> erreurInconnue(); </script>";
 }
 
