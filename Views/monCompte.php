f303a281635a11c685b3b8c1c8b12b742101e132
Julie Choquet <julie.choquet@ensiie.fr>
2016-12-02 15:54:10 +0100

    affichage des differents block sur mon compte en js

--- Views/monCompte.php
+++ Views/monCompte.php
@@ -30,6 +30,27 @@ include "../Models/monProfil.php";
     <script src="../Scripts/bootstrap.js"></script>
     <script src="../Scripts/profil.js"></script>
 
+    <script>
+        (function($) {
+            "use strict";
+
+            $('.page-step').hide();
+            $('#page-step-0').show();
+
+            $(document).on('click', 'a[data-page-step]', function(e) {
+
+              var $this   = $(this)
+              var $target = $this.attr('data-page-step')
+
+              $('.page-step').hide();
+              $($target).show();
+
+              $("ul.page-step-menu li a[data-page-step]").removeClass("active");
+              $("ul.page-step-menu li a[data-page-step='"+$target+"']").addClass("active");
+
+            });
+        })(jQuery);
+    </script>
 </head>
 
 <body>
@@ -50,109 +71,119 @@ $code_postale = get_commune($id_commune);
 ?>
 
 
-<div class="container">
-    <div class="row profile">
-        <div class="col-md-3">
-            <div class="profile-sidebar">
-                <!-- SIDEBAR USERPIC -->
-                <div class="profile-userpic">
-                    <img src="../Images/Objets/<?php echo $path_photo ?>" class="img-responsive" alt="">
+<div class="container-fluid content">
+    <div class="row">
+        <div class="col-sm-3 profile-sidebar">
+
+            <!-- SIDEBAR USERPIC -->
+            <div class="profile-userpic">
+                <img src="../Images/Users/<?php echo $path_photo ?>" class="img-responsive" alt="">
+            </div>
+            <!-- END SIDEBAR USERPIC -->
+
+            <!-- SIDEBAR USER TITLE -->
+            <div class="profile-usertitle">
+                <div class="profile-usertitle-name">
+                    <?php echo $username; ?>
                 </div>
-                <!-- END SIDEBAR USERPIC -->
-                <!-- SIDEBAR USER TITLE -->
-                <div class="profile-usertitle">
-                    <div class="profile-usertitle-name">
-                        <?php echo $username; ?>
-                    </div>
-                    <div class="profile-usertitle-job">
-                        <?php echo $nom; ?><?php echo " " ?><?php echo $prenom; ?>
-                    </div>
+                <div class="profile-usertitle-job">
+                    <?php echo $nom; ?><?php echo " " ?><?php echo $prenom; ?>
                 </div>
-                <!-- END SIDEBAR USER TITLE -->
-                <!-- SIDEBAR BUTTONS -->
-                <div class="profile-userbuttons">
-                    <button type="button" class="btn btn-success btn-sm" href="../Views/monCompte.php">Modifier</button>
-                    <button type="button" class="btn btn-danger btn-sm">Supprimer</button>
-                </div>
-                <!-- END SIDEBAR BUTTONS -->
-                <!-- SIDEBAR MENU -->
-                <div class="profile-usermenu">
-                    <ul class="nav">
-                        <li>
-                            <a href="../Views/profil.php">
-                                <i class="glyphicon glyphicon-home"></i>
-                                Overview </a>
-                        </li>
-                        <li class="active">
-                            <a href="../Views/monCompte.php">
-                                <i class="glyphicon glyphicon-user"></i>
-                                Account Settings </a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- END MENU -->
+            </div>
+            <!-- END SIDEBAR USER TITLE -->
+
+            <!-- SIDEBAR BUTTONS -->
+            <div class="profile-userbuttons">
+                <button type="button" class="btn btn-success btn-sm" href="../Views/monCompte.php">Modifier</button>
+                <button type="button" class="btn btn-danger btn-sm">Supprimer</button>
+            </div>
+            <!-- END SIDEBAR BUTTONS -->
+
+            <!-- SIDEBAR MENU -->
+            <div class="profile-usermenu">
+                <ul class="nav page-step-menu">
+                    <li>
+                        <a href="#" data-page-step="#page-step-1"><i class="glyphicon glyphicon-home"></i>Mon compte</a>
+                    </li>
+                    <li>
+                        <a href="#" data-page-step="#page-step-2"><i class="glyphicon glyphicon-user"></i>Modifier mes informations</a>
+                    </li>
+                    <li>
+                        <a href="#" data-page-step="#page-step-3"><i class="glyphicon glyphicon-user"></i>Suppression</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- END MENU -->
+        </div>
+
+        <!-- Contenu pour afficher le profil -->
+        <div class="col-sm-9 page-step" id="page-step-1">
+            <div class="profile-content">
+            <p>TODO</p>
             </div>
         </div>
-        <div class="col-md-9">
+
+        <!-- Contenu pour modifier son profil -->
+        <div class="col-sm-9 page-step" id="page-step-2">
             <div class="profile-content">
                 <form role="form" id="modifProf" method="POST" action="../Controls/verificationProfil.php">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-6 col-md-6">
-                        <div class="form-group">
-                            <label  for="omdp" > Nom : </label>
-                            <input type="text" name="nom" id="nom" class="form-control input-lg" Value="<?php echo $nom; ?>" placeholder="Nom" tabindex="1" required/>
-                            <span class="errors" id="nomerror"></span>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <label  for="omdp" > Nom : </label>
+                                <input type="text" name="nom" id="nom" class="form-control input-lg" Value="<?php echo $nom; ?>" placeholder="Nom" tabindex="1" required/>
+                                <span class="errors" id="nomerror"></span>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <label  for="omdp" > Prenom : </label>
+                                <input type="text" name="prenom" id="prenom" class="form-control input-lg" Value="<?php echo $prenom; ?>" placeholder="Prenom" tabindex="2" required/>
+                                <span class="errors" id="prenomerror"></span>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-6 col-md-6">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-2 col-md-6">
                         <div class="form-group">
-                            <label  for="omdp" > Prenom : </label>
-                            <input type="text" name="prenom" id="prenom" class="form-control input-lg" Value="<?php echo $prenom; ?>" placeholder="Prenom" tabindex="2" required/>
-                            <span class="errors" id="prenomerror"></span>
+                            <label  for="omdp" > Username : </label>
+                            <input type="text" name="username" id="username" class="form-control input-lg" Value="<?php echo $username; ?>" placeholder="username" tabindex="3" required/>
+                            <span class="errors" id="usernameerror"></span>
+                        </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div class="form-group">
+                            <label  for="omdp" > E-mail : </label>
+                            <input type="email" name="email" id="email" class="form-control input-lg" Value="<?php echo $email; ?>" placeholder="Email" tabindex="4">
+                            <span class="errors" id="emailerror"></span>
+                        </div>
                         </div>
                     </div>
-                </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-2 col-md-6">
-                <div class="form-group">
-                    <label  for="omdp" > Username : </label>
-                    <input type="text" name="username" id="username" class="form-control input-lg" Value="<?php echo $username; ?>" placeholder="username" tabindex="3" required/>
-                    <span class="errors" id="usernameerror"></span>
-                </div>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                <div class="form-group">
-                    <label  for="omdp" > E-mail : </label>
-                    <input type="email" name="email" id="email" class="form-control input-lg" Value="<?php echo $email; ?>" placeholder="Email" tabindex="4">
-                    <span class="errors" id="emailerror"></span>
-                </div>
-                </div>
-            </div>
-                <div class="form-group">
-                    <label  for="omdp" > Adresse : </label>
-                    <input type="text" name="adresse" id="adresse" class="form-control input-lg" Value="<?php echo $adresse; ?>" placeholder="Adresse" tabindex="5">
-                    <span class="errors" id="adresseerror"></span>
-                </div>
+                    <div class="form-group">
+                        <label  for="omdp" > Adresse : </label>
+                        <input type="text" name="adresse" id="adresse" class="form-control input-lg" Value="<?php echo $adresse; ?>" placeholder="Adresse" tabindex="5">
+                        <span class="errors" id="adresseerror"></span>
+                    </div>
 
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                <div class="form-group">
-                    <label  for="omdp" > Numéro de téléphone  : </label>
-                    <input type="text" name="phone" id="phone" class="form-control input-lg" Value="<?php echo $numero_telephone; ?>" placeholder="Numéro de téléphone" tabindex="6">
-                    <span class="errors" id="phoneerror"></span>
-                </div>
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-6">
-                <div class="form-group">
-                    <label  for="omdp" > Commune : </label>
-                    <input type="text" name="commune" id="commune" class="form-control input-lg" Value="<?php echo $code_postale; ?>" placeholder="Commune" tabindex="8">
-                    <span class="errors" id="communeerror"></span>
-                    <datalist id="commune">
-                        <?php include __DIR__ . "/../Models/autocomplete_commune.php"; ?>
-                    </datalist>
-                </div>
-                </div>
-            </div>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div class="form-group">
+                            <label  for="omdp" > Numéro de téléphone  : </label>
+                            <input type="text" name="phone" id="phone" class="form-control input-lg" Value="<?php echo $numero_telephone; ?>" placeholder="Numéro de téléphone" tabindex="6">
+                            <span class="errors" id="phoneerror"></span>
+                        </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div class="form-group">
+                            <label  for="omdp" > Commune : </label>
+                            <input type="text" name="commune" id="commune" class="form-control input-lg" Value="<?php echo $code_postale; ?>" placeholder="Commune" tabindex="8">
+                            <span class="errors" id="communeerror"></span>
+                            <datalist id="commune">
+                                <?php include __DIR__ . "/../Models/autocomplete_commune.php"; ?>
+                            </datalist>
+                        </div>
+                        </div>
+                    </div>
 
                     <div class="form-group">
                         <label  for="omdp" >Ancien mot de passe : </label>
@@ -177,36 +208,41 @@ $code_postale = get_commune($id_commune);
                         </div>
                     </div>
 
-                <div class="form-group">
-                    <div class="col-md-12">
-                        <img src="../Images/Objets/<?php echo $path_photo ?>"  width="150" height="150">
-                        <div class="input-group">
-                            <label class="input-group-btn" for="photo">
-                                <span class="btn btn-info"><span class="glyphicon glyphicon-picture"></span>
-                                    Sélectionner une photo <input type="file" id="photo" name="photo" style="display: none;"/>
-                                </span>
-                            </label>
-                            <input type="text" class="form-control" readonly="" value="Aucun fichier choisi" />
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <img src="../Images/Objets/<?php echo $path_photo ?>"  width="150" height="150">
+                            <div class="input-group">
+                                <label class="input-group-btn" for="photo">
+                                    <span class="btn btn-info"><span class="glyphicon glyphicon-picture"></span>
+                                        Sélectionner une photo <input type="file" id="photo" name="photo" style="display: none;"/>
+                                    </span>
+                                </label>
+                                <input type="text" class="form-control" readonly="" value="Aucun fichier choisi" />
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="form-group">
-                <div class="profile-userbuttons">
-                    <button type="submit" class="btn btn-md btn-default btn-success" id="modifProf" name="modifProf">Modifier le profil</button>
-                    <span class="errors" id="formerror"></span>
-                    <button type="button" class="btn btn-md btn-default btn-danger" data-dismiss="modal" id="dismiss-button2">Annuler</button>
-                </div>
-                </div>
-            </form>
+                    <div class="form-group">
+                    <div class="profile-userbuttons">
+                        <button type="submit" class="btn btn-md btn-default btn-success" id="modifProf" name="modifProf">Modifier le profil</button>
+                        <span class="errors" id="formerror"></span>
+                        <button type="button" class="btn btn-md btn-default btn-danger" data-dismiss="modal" id="dismiss-button2">Annuler</button>
+                    </div>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-</div>
-<br>
-<br>
+
+        <!-- Contenu pour supprimer son compte -->
+        <div class="col-sm-9 page-step" id="page-step-3">
+            <div class="profile-content">
+            <p>TODO</p>
+            </div>
+        </div>
 
 
+    </div> <!-- fermeture du row general -->
+</div> <!-- fermeture du content -->
 
 </body>
 </html>
