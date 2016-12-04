ce6c2bfa9e730ab55b2a3ea2c2dd6fed7b8c2143
Julie Choquet <julie.choquet@ensiie.fr>
2016-12-04 15:01:53 +0100

    templat pour page de profil statique

---
+++ Views/view_profil.php
@@ -0,0 +1,38 @@
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Lend it - Profil</title>
+    <meta name="description" content="Page de Profil">
+
+    <!-- Source CSS Bootstrap -->
+    <link rel="stylesheet" href="Styles/bootstrap.css">
+
+    <!-- Source css pour le design du site -->
+    <link rel="stylesheet" href="Styles/base.css">
+
+    <!-- Source pour le formulaire dajout -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Source jquery -->
+    <script src="Scripts/jquery_library.js"></script>
+    <!-- Source JavaScript Bootstrap -->
+    <script src="Scripts/bootstrap.js"></script>
+
+</head>
+
+<body>
+
+<?php include("Views/header.php"); ?>
+<?php $username = $_GET['username']; ?>
+
+
+
+<div class="container-fluid content">
+  <p> Profil de <?php echo $username; ?></p>
+</div>
+
+</body>
+</html>
