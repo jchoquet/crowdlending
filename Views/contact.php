a2c69a822317f0efd34832b01c9264c29fac206c
Julie Choquet <julie.choquet@ensiie.fr>
2016-12-09 17:17:19 +0100

    template page contact

---
+++ Views/contact.php
@@ -0,0 +1,55 @@
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Lend it - Nous contacter</title>
+
+    <!-- Source CSS Bootstrap -->
+    <link rel="stylesheet" href="Styles/bootstrap.css">
+
+    <!-- Source css pour le design du site -->
+    <link rel="stylesheet" href="Styles/base.css">
+
+    <!-- Source css pour la page-->
+    <link rel="stylesheet" href="Styles/contact.css">
+
+    <!-- Source jquery -->
+    <script src="Scripts/jquery_library.js"></script>
+
+    <!-- Source JavaScript Bootstrap -->
+    <script src="Scripts/bootstrap.js"></script>
+
+</head>
+
+<body>
+
+<?php include("Views/header.php"); ?>
+
+<!-- <div class="container content">
+    <div class="row">
+      <div class="col-md-2"> </div>
+      <div class="col-md-8">
+        <h1>Nous contacter</h1>
+        <p class="lead">Vous avez-une question, une réclamation ou vous souhaitez simplement obtenir plus d'informations ?</p>
+        <p>Vous pouvez contacter l'équipe de modération / administration aux coordonnées suivantes</p>
+    </div>
+    </div>
+</div> -->
+
+    <div class="container content">
+
+        <div class="heading">
+            <h2>Nous <strong>contacter</strong></h2>
+        </div>
+
+        <p class="lead">Vous avez une question, une réclamation ou vous souhaitez simplement obtenir plus d'informations ?</p>
+        <p>Vous pouvez contacter l'équipe de modération / administration aux coordonnées suivantes :</p>
+
+    </div>
+
+<?php include ("Views/footer.php"); ?>
+
+</body>
+</html>
